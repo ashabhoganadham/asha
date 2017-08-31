2392633253315d99f704109d63de8f42d2896fe9
ashabhoganadham <bhoganadham.asha@gmail.com>
2017-08-31 16:19:49 +0530

    add a new selenium project
    add a new selenium project

---
+++ selenium project/src/com/bank/icici/BrowserLauncher.java
@@ -0,0 +1,11 @@
+package com.bank.icici;
+
+public class BrowserLauncher {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+System.out.println("hello ravi sir");
+	}
+	
+}
